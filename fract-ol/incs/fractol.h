927420d9af10134b149c517037d5ff099661daa6
xcharra <xcharrapb@gmail.com>
2023-01-06 17:47:29 +0100

    fdj

--- fract-ol/incs/fractol.h
+++ fract-ol/incs/fractol.h
@@ -6,7 +6,7 @@
 /*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:50:21 by xcharra           #+#    #+#             */
-/*   Updated: 2023/01/05 16:05:09 by xcharra          ###   ########lyon.fr   */
+/*   Updated: 2023/01/06 15:27:52 by xcharra          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,7 @@
 # include <math.h>
 # include "libft.h"
 # include <mlx.h>
+# define WIDTH	500 /* x */
+# define HEIGHT	500 /* y */
 
 #endif
--- fract-ol/srcs/fractol.c
+++ fract-ol/srcs/fractol.c
@@ -6,21 +6,42 @@
 /*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:50:24 by xcharra           #+#    #+#             */
-/*   Updated: 2023/01/06 10:25:07 by xcharra          ###   ########lyon.fr   */
+/*   Updated: 2023/01/06 16:18:32 by xcharra          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-int	main(int argc, char **argv)
+typedef struct s_data
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_data;
+
+void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+{
+	char	*dst;
+
+	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
+	*(unsigned int *)dst = color;
+}
+
+int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
+	t_data	img;
 
-	(void)argc;
-	(void)argv;
 	mlx = mlx_init();
-	mlx_win = mlx_new_window(mlx, 1000, 1000, "Hello world!");
+	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "fract-ol");
+	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
+	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
+	&img.line_length, &img.endian);
+	my_mlx_pixel_put(&img, WIDTH / 2, HEIGHT / 2, 0x00FFFF00);
+	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
+	mlx
 	mlx_loop(mlx);
-	return (1);
 }
