bb8fbb22ce7e45af6bb1e261c29bcb53537fb875
xcharra <xcharrapb@gmail.com>
2023-01-09 18:01:01 +0100

    julia set begin ok

--- fract-ol/incs/fractol.h
+++ fract-ol/incs/fractol.h
@@ -6,7 +6,7 @@
 /*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:50:21 by xcharra           #+#    #+#             */
-/*   Updated: 2023/01/06 15:27:52 by xcharra          ###   ########lyon.fr   */
+/*   Updated: 2023/01/09 17:56:53 by xcharra          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,29 @@
 
 # include <unistd.h>
 # include <stdlib.h>
+# include <stdio.h>
 // # include <limits.h>
 # include <math.h>
 # include "libft.h"
 # include <mlx.h>
-# define WIDTH	500 /* x */
-# define HEIGHT	500 /* y */
+# define WIDTH	1920 /* x */
+# define HEIGHT	1080 /* y */
+# define IMAX	1000
+
+typedef struct s_img
+{
+	void	*img;
+	char	*addr;
+	int		bits_per_pixel;
+	int		line_length;
+	int		endian;
+}			t_img;
+
+typedef struct s_complex
+{
+	double	x;
+	double	xtemp;
+	double	y;
+}			t_complex;
 
 #endif
--- fract-ol/srcs/fractol.c
+++ fract-ol/srcs/fractol.c
@@ -6,22 +6,13 @@
 /*   By: xcharra <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/05 14:50:24 by xcharra           #+#    #+#             */
-/*   Updated: 2023/01/06 16:18:32 by xcharra          ###   ########lyon.fr   */
+/*   Updated: 2023/01/09 17:59:02 by xcharra          ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fractol.h"
 
-typedef struct s_data
-{
-	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}			t_data;
-
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
+void	my_mlx_pixel_put(t_img *data, int x, int y, int color)
 {
 	char	*dst;
 
@@ -29,19 +20,70 @@ void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
 	*(unsigned int *)dst = color;
 }
 
+double	zmod2(t_complex *pixels)
+{
+	return (pow(pixels->x, 2) + pow(pixels->y, 2));
+}
+
+double	xjulia(t_complex *pixels, double cx)
+{
+	return (pow(pixels->x, 2) - pow(pixels->y, 2) + cx);
+}
+
+double	yjulia(t_complex *pixels, double cy)
+{
+	return (2 * pixels->x * pixels->y + cy);
+}
+
+void	julia_set(t_img *img)
+{
+	t_complex	pix;
+	t_complex	z;
+	t_complex	c;
+	size_t		i;
+	double		range;
+
+	pix.x = 0;
+	c.x = -0.4;
+	c.y = 0.6;
+	range = 1.5;
+	while (pix.x < WIDTH)
+	{
+		pix.y = 0;
+		while (pix.y < HEIGHT)
+		{
+			z.x = ((pix.x / (WIDTH / (range * 2))) - range) * WIDTH / HEIGHT;
+			z.y = range - (pix.y / (HEIGHT / (range * 2)));
+			i = 0;
+			while (zmod2(&z) < 4. && i < IMAX)
+			{
+				z.xtemp = xjulia(&z, c.x);
+				z.y = yjulia(&z, c.y);
+				z.x = z.xtemp;
+				i++;
+			}
+			if (i == IMAX)
+				my_mlx_pixel_put(img, pix.x, pix.y, 0x0068c2d3);
+			else
+				my_mlx_pixel_put(img, pix.x, pix.y, 0x00a2dcc7 * i / 10000);
+			pix.y++;
+		}
+		pix.x++;
+	}
+}
+
 int	main(void)
 {
 	void	*mlx;
 	void	*mlx_win;
-	t_data	img;
+	t_img	img;
 
 	mlx = mlx_init();
 	mlx_win = mlx_new_window(mlx, WIDTH, HEIGHT, "fract-ol");
 	img.img = mlx_new_image(mlx, WIDTH, HEIGHT);
 	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, \
 	&img.line_length, &img.endian);
-	my_mlx_pixel_put(&img, WIDTH / 2, HEIGHT / 2, 0x00FFFF00);
+	julia_set(&img);
 	mlx_put_image_to_window(mlx, mlx_win, img.img, 0, 0);
-	mlx
 	mlx_loop(mlx);
 }
